dc7f97f98dbad9f39fcb8736ebdbbd6b6b141592
IgorMijanovic <igor.mijanovic02@gmail.com>
2025-02-24 19:33:03 +0100

    Add dropdown menu in footer

--- src/components/ui/sidebar/footer-sidebar.tsx
+++ src/components/ui/sidebar/footer-sidebar.tsx
@@ -9,4 +9,20 @@ import {
     AvatarFallback,
     AvatarImage,
   } from "@/components/ui/avatar"
-  
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  import {
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    useSidebar,
+  } from "@/components/ui/sidebar"
+
+ 
