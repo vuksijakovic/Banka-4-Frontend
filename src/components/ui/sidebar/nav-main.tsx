686d496aef6833fe100d25ae4fc9fba4d45a2cab
IgorMijanovic <igor.mijanovic02@gmail.com>
2025-02-24 18:33:42 +0100

    Import sidebar in nav-main

--- src/components/ui/sidebar/nav-main.tsx
+++ src/components/ui/sidebar/nav-main.tsx
@@ -1,5 +1,16 @@
+
 import {
     Collapsible,
     CollapsibleContent,
     CollapsibleTrigger,
   } from "@/components/ui/collapsible"
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
