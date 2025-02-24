f0233c8b0520b6da49aeb50c0a65882c2c42920b
IgorMijanovic <igor.mijanovic02@gmail.com>
2025-02-24 18:57:28 +0100

    Import needed components in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import { AppSidebar } from "@/components/ui/sidebar/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
 export default function Home() {
-  return <h1>Landing page</h1>;
+
+  return 
+  
+  <h1>Landing page</h1>;
 }
