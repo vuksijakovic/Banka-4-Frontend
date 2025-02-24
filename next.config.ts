38bb4e46abbcd4206de9996bef4dbaf6d7282346
Marko Kocic <markokocic2000@gmail.com>
2025-02-24 20:13:39 +0100

    Add path rewrites for set/reset password

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,33 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/i/s',
+                has: [
+                    {
+                        type: 'query',
+                        key: 't',
+                        value: '(?<t>.*)'
+                    }
+                ],
+                destination: '/auth/password/set/:t'
+            },
+            {
+                source: '/i/r',
+                has: [
+                    {
+                        type: 'query',
+                        key: 't',
+                        value: '(?<t>.*)'
+                    }
+                ],
+                destination: '/auth/password/reset/:t'
+            }
+        ]
+    },
+}
+
 export default nextConfig;
