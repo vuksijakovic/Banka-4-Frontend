1071c0e0d202eeb83a0313a6cae1a32b0eea84a5
umilosev <umilosev12822rn@raf.rs>
2025-02-24 22:42:48 +0100

    Revert "Put in tests for the employee overview page"
    This reverts commit afe51b6d9587fc43048d73bc9aed8bf639f2e2f5.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: '3qv7gb',
   e2e: {
-    baseUrl: 'http://localhost:3000', // Replace with your application's base URL
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
