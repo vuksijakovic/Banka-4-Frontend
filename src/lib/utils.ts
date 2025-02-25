f081adb9e3bc46e50af2a1c722fa81d1fba96b3c
Arsen Arsenović <arsen@aarsen.me>
2025-02-25 01:22:43 +0100

    utils: define an API_BASE URL constant

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? '/undefined/api';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
