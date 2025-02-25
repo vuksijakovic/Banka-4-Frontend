1969d72e0fb22039bf93dbd6530d76f9c2c87bb0
IgorMijanovic <igor.mijanovic02@gmail.com>
2025-02-25 02:15:02 +0100

    Add /employee

--- src/components/ui/sidebar/app-sidebar.tsx
+++ src/components/ui/sidebar/app-sidebar.tsx
@@ -36,7 +36,7 @@ const data = {
       items: [
         {
           title: 'Overview',
-          url: 'employee/overview',
+          url: '/employee/overview',
           icon: List,
         },
         {
