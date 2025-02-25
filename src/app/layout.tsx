c6f7ab6d30a9d1b9ef78de9d61d81611cf991ee4
vsijakovic1421rn <vsijakovic1421rn@raf.rs>
2025-02-25 01:52:41 +0100

    Implement handling of access/refresh tokens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <QueryClientProvider client={queryClient}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -36,6 +39,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+      </QueryClientProvider>
       </body>
     </html>
   );
