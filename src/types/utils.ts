e5854ce6ef6908d2d60372f90bfa44eda8187bb3
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-02-26 18:43:13 +0100

    types/utils: add assertNever for exhaustiveness/dead code checking

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -2,3 +2,12 @@ export type SomePartial<T, Members extends keyof T> = Partial<
   Pick<T, Members>
 > &
   Omit<T, Members>;
+
+/** Check that `value` is a `never` type.  Useful for exhaustiveness and
+ * dead-code checking.
+ * @param value Value that should never be present.
+ * @returns Never returns.  Should never be called.
+ */
+export function assertNever(value: never): never {
+  throw new Error(`Reached unreachable with value ${value}`);
+}
