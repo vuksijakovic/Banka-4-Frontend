e483c6a034b0a508113a62e1d4d9f4b8b6cf2e03
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-02-27 02:23:59 +0100

    login: use mutation for handing login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,28 +2,28 @@
 import { toastRequestError } from '@/api/errors';
 import LoginForm, { LoginFormData } from '@/components/auth/login-form';
 import { useAuth } from '@/context/AuthContext';
+import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function LoginPage() {
   const router = useRouter();
   const auth = useAuth();
+
+  const { isPending, mutate: doLogin } = useMutation({
+    mutationFn: async ({ email, password }: LoginFormData) => {
+      if (!auth.isLoggedIn) await auth.login('employee', email, password);
+    },
+    onError: (err) => toastRequestError(err),
+  });
+
   useEffect(() => {
     if (auth.isLoggedIn) return router.replace('/');
   }, [router, auth]);
 
-  async function handleLogin({ email, password }: LoginFormData) {
-    if (auth.isLoggedIn) return;
-    try {
-      await auth.login('employee', email, password);
-    } catch (err) {
-      toastRequestError(err);
-    }
-  }
-
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <LoginForm onSubmitAction={handleLogin} />
+      <LoginForm enabled={!isPending} onSubmitAction={doLogin} />
     </div>
   );
 }
