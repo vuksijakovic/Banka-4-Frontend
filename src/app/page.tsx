a99d4dff7e7f03e647da7573f6f445c5ab685eab
Marko Kocic <markokocic2000@gmail.com>
2025-02-27 10:42:51 +0100

    landing: add navigation menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { AppNavigationMenu } from '@/components/ui/navbar';
+
 export default function Home() {
-  return <h1>Landing page</h1>;
+  return (
+    <>
+      <AppNavigationMenu />
+      <h1>Landing Page</h1>
+    </>
+  );
 }
