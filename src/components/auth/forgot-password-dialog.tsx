707d1620ebe03ca4adaf53d95ac3bcc26252c2af
Marko Kocic <markokocic2000@gmail.com>
2025-02-27 17:58:17 +0100

    login/page: fix style and forgot password not submitting

--- src/components/auth/forgot-password-dialog.tsx
+++ src/components/auth/forgot-password-dialog.tsx
@@ -5,7 +5,6 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import {
   Dialog,
   DialogContent,
@@ -19,13 +18,23 @@ import { useHttpClient } from '@/context/HttpClientContext';
 import { useMutation } from '@tanstack/react-query';
 import { forgotPassword } from '@/api/employee';
 import { toastRequestError } from '@/api/errors';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
 const forgotPasswordSchema = z.object({
   email: z.string().email(),
 });
 
+type ForgotPasswordValues = z.infer<typeof forgotPasswordSchema>;
+
 export default function ForgotPasswordDialog() {
-  const form = useForm({
+  const form = useForm<ForgotPasswordValues>({
     resolver: zodResolver(forgotPasswordSchema),
     defaultValues: { email: '' },
   });
@@ -37,63 +46,57 @@ export default function ForgotPasswordDialog() {
     onError: (error) => toastRequestError(error),
   });
 
-  const onSubmit = async (data: { email: string }) => {
-    console.log('here');
+  const onSubmit = (data: ForgotPasswordValues) => {
     doForgotPassword(data.email);
   };
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <p className="text-sm text-gray-500 text-right mt-1 cursor-pointer underline hover:underline">
-          Forgot your password?
-        </p>
+        <span className="text-sm text-right w-full text-muted-foreground mt-1 cursor-pointer underline">
+          forgot your password?
+        </span>
       </DialogTrigger>
       <DialogContent className="max-w-sm p-6 rounded-lg shadow-lg">
         <DialogHeader>
           <DialogTitle className="text-2xl font-semibold">
             Forgot your password?
           </DialogTitle>
-          <DialogDescription className="text-sm text-gray-500">
+          <DialogDescription className="text-sm">
             Enter your email address, and we’ll send you a link to reset your
             password.
           </DialogDescription>
         </DialogHeader>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            form.handleSubmit(onSubmit);
-          }}
-          className="grid gap-4"
-        >
-          <div className="grid gap-2">
-            <Label htmlFor="email" className="text-sm font-medium">
-              Email
-            </Label>
-            <Input
-              id="email"
-              type="text"
-              placeholder="example@domain.com"
-              {...form.register('email')}
+        <Form {...form}>
+          <form
+            className={'flex flex-col gap-4'}
+            onSubmit={(e) => {
+              e.stopPropagation();
+              form.handleSubmit(onSubmit)(e);
+              e.preventDefault();
+            }}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="flex flex-col space-y-1.5">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="john@example.com" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            {form.formState.errors.email && (
-              <p className="text-sm text-red-500">
-                {form.formState.errors.email.message}
-              </p>
-            )}
-          </div>
 
-          <DialogFooter>
-            <Button
-              type="submit"
-              disabled={isPending}
-              className="flex justify-end font-normal"
-            >
-              {isPending ? 'Sending...' : 'Send Email'}
-            </Button>
-          </DialogFooter>
-        </form>
+            <div className="flex w-full justify-end">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? 'Sending...' : 'Send Email'}
+              </Button>
+            </div>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
