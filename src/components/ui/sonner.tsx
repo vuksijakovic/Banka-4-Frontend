a34f56294f0e420a6fc2e989b5a1dd755d0b4317
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-02-28 13:24:38 +0100

    toaster: enable rich colors

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -11,6 +11,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps['theme']}
+      richColors={true}
       className="toaster group"
       toastOptions={{
         classNames: {
