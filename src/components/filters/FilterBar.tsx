7f3d82b93fc07195d1dee340fb4985fe9ad1089e
fl1k <vgolubovic97@gmail.com>
2025-03-02 22:37:12 +0100

    Implement filterKeyToName onto FilterBar

--- src/components/filters/FilterBar.tsx
+++ src/components/filters/FilterBar.tsx
@@ -1,51 +1,53 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import FilterInput from './FilterInput';
-import { Button } from '@/components/ui/button';
-import { Search } from 'lucide-react';
+import {Button} from '@/components/ui/button';
+import {Search} from 'lucide-react';
 
 
 interface FilterBarProps<TFilter extends { [K in keyof TFilter]: string }> {
-  onSearch: (filters: TFilter) => void;
-  filters: TFilter;
+    onSearch: (filters: TFilter) => void;
+    filter: TFilter;
+    filterKeyToName: (key: keyof TFilter) => string;
 }
 
 export function FilterBar<TFilter extends { [K in keyof TFilter]: string }>({
-  onSearch,
-  filters,
-}: FilterBarProps<TFilter>) {
-  const [vFilters, setVFilters] = useState<TFilter>(filters);
+                                                                                onSearch,
+                                                                                filter,
+                                                                                filterKeyToName
+                                                                            }: FilterBarProps<TFilter>) {
+    const [filterState, setFilterState] = useState<TFilter>(filter);
 
-  const handleFilterChange = (propertyName: string, newValue: string) => {
-    setVFilters((prevFilters) => ({
-      ...prevFilters,
-      [propertyName]: newValue,
-    }));
-  };
+    const handleFilterChange = (propertyName: string, newValue: string) => {
+        setFilterState((prevFilters) => ({
+            ...prevFilters,
+            [propertyName]: newValue,
+        }));
+    };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onSearch(vFilters);
-  };
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        onSearch(filterState);
+    };
 
-  return (
-    <form onSubmit={handleSubmit} className="flex mb-4 space-x-2">
-      {Object.keys(filters).map((key) => (
-        <FilterInput
-          key={key}
-          propertyName={key}
-          value={filters[key as keyof TFilter]}
-          onChange={handleFilterChange}
-          placeholder={`Filter by ${key}`}
-        />
-      ))}
-      <Button type="submit">
-        Search
-        <Search className="w-4 h-4 mr-1" />
-      </Button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit} className="flex mb-4 space-x-2">
+            {Object.keys(filter).map((key) => (
+                <FilterInput
+                    key={key}
+                    propertyName={key}
+                    value={filter[key as keyof TFilter]}
+                    onChange={handleFilterChange}
+                    placeholder={`Filter by ${filterKeyToName(key as keyof TFilter)}`}
+                />
+            ))}
+            <Button type="submit">
+                Search
+                <Search className="w-4 h-4 mr-1"/>
+            </Button>
+        </form>
+    );
 }
 
 export default FilterBar;
