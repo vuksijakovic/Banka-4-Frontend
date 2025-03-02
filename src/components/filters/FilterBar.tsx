e7aca0a8dcb3cc654ae7cfe536c2f5893224f739
fl1k <vgolubovic97@gmail.com>
2025-03-02 22:27:54 +0100

    Implement FilterInput

---
+++ src/components/filters/FilterBar.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React, { useState } from 'react';
+import FilterInput from './FilterInput';
+import { Button } from '@/components/ui/button';
+import { Search } from 'lucide-react';
+
+interface FilterBarProps<TFilter extends Record<string, string>> {
+  onSearch: (filters: TFilter) => void;
+  filters: TFilter;
+}
+
+export function FilterBar<TFilter extends Record<string, string>>({
+  onSearch,
+  filters,
+}: FilterBarProps<TFilter>) {
+  const [vFilters, setVFilters] = useState<TFilter>(filters);
+
+  const handleFilterChange = (propertyName: string, newValue: string) => {
+    setVFilters((prevFilters) => ({
+      ...prevFilters,
+      [propertyName]: newValue,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearch(vFilters);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex mb-4 space-x-2">
+      {Object.keys(filters).map((key) => (
+        <FilterInput
+          key={key}
+          propertyName={key}
+          value={filters[key as keyof TFilter]}
+          onChange={handleFilterChange}
+          placeholder={`Filter by ${key}`}
+        />
+      ))}
+      <Button type="submit">
+        Search
+        <Search className="w-4 h-4 mr-1" />
+      </Button>
+    </form>
+  );
+}
+
+export default FilterBar;
