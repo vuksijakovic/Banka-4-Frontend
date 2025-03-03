097ae0c023abfc80c7b0e20a071111bae77eb200
aaleksaaleksic <aaleksic9722rn@raf.rs>
2025-03-03 14:56:37 +0100

    run npm check and prettier on last commit

--- src/components/ui/sidebar/app-sidebar.tsx
+++ src/components/ui/sidebar/app-sidebar.tsx
@@ -3,11 +3,11 @@
 import { BriefcaseBusiness, Landmark, List, UserPlus } from 'lucide-react';
 
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarHeader,
-    SidebarRail,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
 } from '@/components/ui/sidebar';
 
 import { NavMain } from '@/components/ui/sidebar/nav-main-sidebar';
@@ -20,92 +20,90 @@ import { useQueryClient } from '@tanstack/react-query';
 import { Privilege, isValidPrivilege } from '@/types/privileges';
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-    const queryClient = useQueryClient();
-    const httpClient = useHttpClient();
-    const auth = useAuth();
-    const me = useMe();
+  const queryClient = useQueryClient();
+  const httpClient = useHttpClient();
+  const auth = useAuth();
+  const me = useMe();
 
-    const onLogout = () => {
-        if (auth.isLoggedIn) {
-            auth.logout();
-            queryClient.invalidateQueries({
-                queryKey: ['employee', 'me'],
-            });
-        }
-    };
+  const onLogout = () => {
+    if (auth.isLoggedIn) {
+      auth.logout();
+      queryClient.invalidateQueries({
+        queryKey: ['employee', 'me'],
+      });
+    }
+  };
 
+  const userPrivileges: Privilege[] =
+    me.state === 'logged-in' ? me.me.privileges.filter(isValidPrivilege) : [];
 
-    const userPrivileges: Privilege[] =
-        me.state === 'logged-in' ? me.me.privileges.filter(isValidPrivilege) : [];
+  const hasRequiredPrivileges = (requiredPrivileges: Privilege[]) => {
+    return requiredPrivileges.every((priv) => userPrivileges.includes(priv));
+  };
 
-
-    const hasRequiredPrivileges = (requiredPrivileges: Privilege[]) => {
-        return requiredPrivileges.every((priv) => userPrivileges.includes(priv));
-    };
-
-    const data = {
-        teams: [
-            {
-                name: 'RAFeisen Bank',
-                logo: Landmark,
-                url: '/',
-            },
+  const data = {
+    teams: [
+      {
+        name: 'RAFeisen Bank',
+        logo: Landmark,
+        url: '/',
+      },
+    ],
+    navMain: [
+      {
+        title: 'Employees',
+        url: '/employee',
+        icon: BriefcaseBusiness,
+        isActive: true,
+        items: [
+          {
+            title: 'Overview',
+            url: '/employee',
+            icon: List,
+            privileges: ['ADMIN', 'SEARCH', 'FILTER'],
+          },
+          {
+            title: 'New',
+            url: '/employee/new',
+            icon: UserPlus,
+            privileges: ['ADMIN', 'CREATE'],
+          },
         ],
-        navMain: [
-            {
-                title: 'Employees',
-                url: '/employee',
-                icon: BriefcaseBusiness,
-                isActive: true,
-                items: [
-                    {
-                        title: 'Overview',
-                        url: '/employee',
-                        icon: List,
-                        privileges: ['ADMIN', 'SEARCH', 'FILTER'],
-                    },
-                    {
-                        title: 'New',
-                        url: '/employee/new',
-                        icon: UserPlus,
-                        privileges: ['ADMIN', 'CREATE'],
-                    },
-                ],
-            },
-        ]
-            .map((section) => {
-                const filteredItems = section.items.filter((item) =>
-                    hasRequiredPrivileges(item.privileges as Privilege[])
-                );
+      },
+    ]
+      .map((section) => {
+        const filteredItems = section.items.filter((item) =>
+          hasRequiredPrivileges(item.privileges as Privilege[])
+        );
 
-                return filteredItems.length > 0
-                    ? { ...section, items: filteredItems }
-                    : null;
-            })
-            .filter((item): item is NonNullable<typeof item> => item !== null),
-    };
+        return filteredItems.length > 0
+          ? { ...section, items: filteredItems }
+          : null;
+      })
+      .filter((item): item is NonNullable<typeof item> => item !== null),
+  };
 
-    return (
-        <Sidebar collapsible="icon" {...props}>
-            <SidebarHeader>
-                <HeaderSidebar teams={data.teams} />
-            </SidebarHeader>
-            <SidebarContent>
-                <NavMain items={data.navMain} />
-            </SidebarContent>
-            {me.state === 'logged-in' && (
-                <SidebarFooter>
-                    <FooterSidebar
-                        onLogoutAction={onLogout}
-                        user={{
-                            name: me.me.firstName,
-                            email: me.me.email,
-                            avatar: '',
-                        }}
-                    />
-                </SidebarFooter>
-            )}
-            <SidebarRail />
-        </Sidebar>
-    );
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <HeaderSidebar teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      {me.state === 'logged-in' && (
+        <SidebarFooter>
+          <FooterSidebar
+            onLogoutAction={onLogout}
+            user={{
+              name: me.me.firstName,
+              email: me.me.email,
+              avatar: '',
+            }}
+          />
+        </SidebarFooter>
+      )}
+      <SidebarRail />
+    </Sidebar>
+  );
 }
