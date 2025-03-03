73fbb27850ad559bd66eec8eac7a00b3d73d8c2b
Marko Kocic <markokocic2000@gmail.com>
2025-03-03 13:03:17 +0100

    login/form: remove card stuff out of component

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -3,13 +3,6 @@
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import {
@@ -47,71 +40,53 @@ export default function LoginForm({
   });
 
   return (
-    <Card className="w-[348px] shadow-md">
-      <CardHeader className="space-y-2">
-        <CardTitle>Login</CardTitle>
-        <CardDescription>
-          Securely access your account and manage your finances with ease.
-        </CardDescription>
-      </CardHeader>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmitAction)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="example@domain.com"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <div className={'w-full text-end'}>
+                <FormControl>
+                  <Input type="password" placeholder="**********" {...field} />
+                </FormControl>
+                <FormMessage className="text-red-500" />
+                <ForgotPasswordDialog />
+              </div>
+            </FormItem>
+          )}
+        />
 
-      <CardContent>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmitAction)}
-            className="space-y-4"
+        <div className="flex justify-end">
+          <Button
+            type="submit"
+            variant="default"
+            className="font-normal"
+            disabled={!enabled}
           >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="text"
-                      placeholder="example@domain.com"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage className="text-red-500" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <div className={'w-full text-end'}>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        placeholder="**********"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage className="text-red-500" />
-                    <ForgotPasswordDialog />
-                  </div>
-                </FormItem>
-              )}
-            />
-
-            <div className="flex justify-end">
-              <Button
-                type="submit"
-                variant="default"
-                className="font-normal"
-                disabled={!enabled}
-              >
-                Login
-              </Button>
-            </div>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 }
