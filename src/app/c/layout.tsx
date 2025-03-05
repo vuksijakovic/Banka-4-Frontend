b097f42ef9a06e240c489a073e00a989789c58bf
aaleksaaleksic <aaleksic9722rn@raf.rs>
2025-03-05 15:42:05 +0100

    fix border and p-4 padding

--- src/app/c/layout.tsx
+++ src/app/c/layout.tsx
@@ -9,27 +9,27 @@ import { AppBreadcrumb } from '@/components/ui/app-breadcrumb';
 import { BreadcrumbProvider } from '@/context/BreadcrumbContext';
 
 export default function SidebarLayout({
-  children,
-}: {
+                                        children,
+                                      }: {
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <BreadcrumbProvider>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <AppBreadcrumb />
-            </div>
-          </header>
-          <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            {children}
-          </main>
-        </BreadcrumbProvider>
-      </SidebarInset>
-    </SidebarProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <BreadcrumbProvider>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <AppBreadcrumb />
+              </div>
+            </header>
+            <main className="flex flex-1 flex-col gap-4">
+              {children}
+            </main>
+          </BreadcrumbProvider>
+        </SidebarInset>
+      </SidebarProvider>
   );
 }
