ab726012168a6779b51b44574202c2de16fcbf47
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-03-06 23:06:47 +0100

    TransactionDialog: allow printing
    we synthesize an iframe and call 'print' on it when we feel like it
    
    the iframe lacks our css, so I needed to provide a way to customize how
    the details of a transaction are rendered in order to use something easy
    to style, like table, rather than tailwind utility classes
    
    this is ugly af

---
+++ src/components/transaction/TransactionDetails.tsx
@@ -0,0 +1,138 @@
+import {
+  paymentStatusToString,
+  TransactionDto,
+} from '@/api/response/transaction';
+import { Label } from '@/components/ui/label';
+import { Badge } from '@/components/ui/badge';
+import { cn, formatDateTime } from '@/lib/utils';
+
+/** Formatter for {@link TransactionDetails}.
+ *  @param p.children - value of the field.
+ *  @param p.name - name of the field.
+ *  @param p.isFirst - whether this field is the first field in the output.
+ */
+export type TXDetailsFieldFormatter = (p: {
+  children: React.ReactNode;
+  name: string;
+  isFirst: boolean;
+}) => React.ReactNode;
+
+const defaultFieldFormatter: TXDetailsFieldFormatter = ({
+  children,
+  name,
+  isFirst,
+}) => (
+  <div className={cn(isFirst && 'col-span-2')}>
+    <Label className="text-muted-foreground row-span-2">{name}</Label>
+    {children}
+  </div>
+);
+
+/** Render a transaction using a customizable formatter.  Kinda an ugly
+ *  solution to be honest.
+ *  @param dto - transaction to display.
+ *  @param fieldFormatter - how to format each transaction field
+ *  @param raw - if false, wraps the body in a two-column grid
+ */
+export default function TransactionDetails({
+  dto,
+  fieldFormatter = defaultFieldFormatter,
+  raw = false,
+}: {
+  dto: TransactionDto;
+  fieldFormatter?: TXDetailsFieldFormatter;
+  raw?: boolean;
+}) {
+  const rawRows = (
+    <>
+      {fieldFormatter({
+        name: 'Transaction number',
+        children: <p>{dto.transactionNumber}</p>,
+        isFirst: true,
+      })}
+      {fieldFormatter({
+        name: 'From account number',
+        children: <></>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'From account number',
+        children: <p>{dto.fromAccount}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'To account number',
+        children: <p>{dto.toAccount}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'From amount',
+        children: <p>{dto.fromAmount}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'From currency',
+        children: <p>{dto.fromCurrency}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'To amount',
+        children: <p>{dto.toAmount}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'To currency',
+        children: <p>{dto.toCurrency}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'Fee amount',
+        children: <p>{dto.feeAmount}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'Fee currency',
+        children: <p>{dto.feeCurrency}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'Recipient name',
+        children: <p>{dto.recipient}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'Payment code',
+        children: <p>{dto.paymentCode}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'Reference number',
+        children: <p>{dto.referenceNumber}</p>,
+        isFirst: false,
+      })}
+      {dto.paymentPurpose &&
+        fieldFormatter({
+          name: 'Payment purpose',
+          children: <p>{dto.paymentPurpose}</p>,
+          isFirst: false,
+        })}
+      {fieldFormatter({
+        name: 'Payment date and time',
+        children: <p>{formatDateTime(dto.paymentDateTime)}</p>,
+        isFirst: false,
+      })}
+      {fieldFormatter({
+        name: 'Payment status',
+        children: (
+          <Badge className="block mt-2 justify-center mr-auto w-min">
+            {paymentStatusToString(dto.status).toUpperCase()}
+          </Badge>
+        ),
+        isFirst: false,
+      })}
+    </>
+  );
+  if (raw) return rawRows;
+
+  return <div className="grid grid-cols-2 gap-y-4">{rawRows}</div>;
+}
--- src/components/transaction/TransactionDialog.tsx
+++ src/components/transaction/TransactionDialog.tsx
@@ -7,13 +7,10 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
-import {
-  paymentStatusToString,
-  TransactionDto,
-} from '@/api/response/transaction';
-import { Label } from '../ui/label';
-import { Badge } from '../ui/badge';
-import { formatDateTime } from '@/lib/utils';
+import { TransactionDto } from '@/api/response/transaction';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import TransactionDetails from './TransactionDetails';
 
 export const TransactionDialog = ({
   open,
@@ -24,6 +21,12 @@ export const TransactionDialog = ({
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const [printerWindow, setPrinterWindow] = useState<Window | null>(null);
+
+  function doPrint() {
+    printerWindow?.print();
+  }
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
@@ -33,76 +36,34 @@ export const TransactionDialog = ({
             Details about this transaction are shown in the following form.
           </DialogDescription>
         </DialogHeader>
-        <div>
-          <Label className={'text-muted-foreground'}>Transaction number</Label>
-          <p>{dto.transactionNumber}</p>
-        </div>
-        <div className="grid grid-cols-2 gap-y-4">
-          <div>
-            <Label className={'text-muted-foreground'}>
-              From account number
-            </Label>
-            <p>{dto.fromAccount}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>To account number</Label>
-            <p>{dto.toAccount}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>From amount</Label>
-            <p>{dto.fromAmount}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>From currency</Label>
-            <p>{dto.fromCurrency}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>To amount</Label>
-            <p>{dto.toAmount}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>To currency</Label>
-            <p>{dto.toCurrency}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>Fee amount</Label>
-            <p>{dto.feeAmount}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>Fee currency</Label>
-            <p>{dto.feeCurrency}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>Recipient name</Label>
-            <p>{dto.recipient}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>Payment code</Label>
-            <p>{dto.paymentCode}</p>
-          </div>
-          <div>
-            <Label className={'text-muted-foreground'}>Reference number</Label>
-            <p>{dto.referenceNumber}</p>
-          </div>
-          {dto.paymentPurpose && (
-            <div>
-              <Label className={'text-muted-foreground'}>Payment purpose</Label>
-              <p>{dto.paymentPurpose}</p>
-            </div>
-          )}
-          <div>
-            <Label className={'text-gray-500'}>Payment date and time</Label>
-            <p>{formatDateTime(dto.paymentDateTime)}</p>
-          </div>
-          <div className="flex flex-col">
-            <Label className={'text-gray-500'}>Payment status</Label>
-            <Badge className="mt-2 justify-center mr-auto">
-              {paymentStatusToString(dto.status).toUpperCase()}
-            </Badge>
-          </div>
-        </div>
+        <TransactionDetails dto={dto} />
+        <iframe
+          className="hidden"
+          onLoad={(e) => setPrinterWindow(e.currentTarget.contentWindow)}
+        />
         <div className="flex flex-col-reverse gap-2 sm:flex-row-reverse">
-          <Button>Print</Button>
+          {printerWindow?.document.body &&
+            createPortal(
+              <>
+                <style>{`td { border-bottom: 0.1mm solid black; }`}</style>
+                <table>
+                  <tbody>
+                    <TransactionDetails
+                      dto={dto}
+                      fieldFormatter={({ name, children }) => (
+                        <tr>
+                          <td>{name}</td>
+                          <td>{children}</td>
+                        </tr>
+                      )}
+                      raw={true}
+                    />
+                  </tbody>
+                </table>
+              </>,
+              printerWindow.document.body
+            )}
+          <Button onClick={doPrint}>Print</Button>
           <DialogClose asChild>
             <Button type="button" variant="secondary">
               Close
