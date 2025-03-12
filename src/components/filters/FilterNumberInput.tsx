35a495b1d2ada707d7e7dccf4c4d531fa36b371c
7ampon <102767875+7ampon@users.noreply.github.com>
2025-03-12 22:18:47 +0100

    add number filter (#160)

--- src/components/filters/FilterNumberInput.tsx
+++ src/components/filters/FilterNumberInput.tsx
@@ -1,20 +1,42 @@
 'use client';
 
+import React from 'react';
+import { Input } from '@/components/ui/input';
+import { MaybePromise } from '@/types/MaybePromise';
+
 export interface FilterNumberInputProps<TFilterKey> {
   propertyName: TFilterKey;
-  value: number;
-  onChange: (propertyName: TFilterKey, newValue: number) => void;
+  value: number | null;
+  onChange: (
+    propertyName: TFilterKey,
+    newValue: number | null
+  ) => MaybePromise<unknown>;
   placeholder?: string;
 }
 
-// TODO: implement
 const FilterNumberInput = <TFilterKey,>({
   propertyName,
   value,
   onChange,
   placeholder,
 }: FilterNumberInputProps<TFilterKey>) => {
-  return null;
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const parsed = parseFloat(e.target.value);
+    await onChange(propertyName, isNaN(parsed) ? null : parsed);
+  };
+
+  return (
+    <div className="filter-input w-full">
+      <Input
+        className="w-full"
+        type="number"
+        name={String(propertyName)}
+        placeholder={placeholder || `Filter by ${propertyName}`}
+        value={value ?? ''}
+        onChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export default FilterNumberInput;
