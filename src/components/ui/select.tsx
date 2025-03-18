aef36667c5d3425ce33b52d8db40a7ca799a7265
unknown <vsijakovic1421rn@raf.rs>
2025-03-18 20:27:17 +0100

    Fix select checkmark

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -123,7 +123,7 @@ const SelectItem = React.forwardRef<
     )}
     {...props}
   >
-    <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
+    <span className="flex h-3.5 w-3.5 items-center justify-center absolute right-2 ">
       <SelectPrimitive.ItemIndicator>
         <Check className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
