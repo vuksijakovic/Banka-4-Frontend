5e6353755423dc53916fcd079776699d17f39d98
IgorMijanovic <igor.mijanovic02@gmail.com>
2025-03-19 19:47:18 +0100

    Update exchange data table

--- src/app/c/exchange/page.tsx
+++ src/app/c/exchange/page.tsx
@@ -13,28 +13,21 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { ArrowLeftRight } from 'lucide-react';
-
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { exchange } from '@/api/response/exchange';
-
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from '@tanstack/react-table';
-
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
+import useTablePageParams from '@/hooks/useTablePageParams';
+import { useHttpClient } from '@/context/HttpClientContext';
+import { searchExchanges } from '@/api/exchange';
+import { useQuery } from '@tanstack/react-query';
+import { DataTable } from '@/components/dataTable/DataTable';
 
 const ExchangePage: React.FC = () => {
+  const { page, pageSize, setPage, setPageSize } = useTablePageParams(
+    'accounts',
+    { pageSize: 8, page: 0 }
+  );
+
   const { dispatch } = useBreadcrumb();
   useEffect(() => {
     dispatch({
@@ -59,103 +52,34 @@ const ExchangePage: React.FC = () => {
 
   const [exchangeData, setExchangeData] = useState<exchange[]>([]);
 
-  const fetchExchangeRate = async () => {
-    const x = await fetch('http://localhost:8080/exchange/exchange-rate');
-    const y = await x.json();
-    const { exchanges } = y;
+  const client = useHttpClient();
+  const { data, isLoading } = useQuery({
+    queryKey: ['exchange', page, pageSize],
+    queryFn: async () => {
+      const response = await searchExchanges(client, pageSize, page);
+      console.log(response.data);
 
-    const formattedExchanges = Object.keys(exchanges).map((key) => ({
-      Base: exchanges[key].Base,
-      Quote: exchanges[key].Quote,
-      Buy: exchanges[key].Buy,
-      Neutral: exchanges[key].Neutral,
-      Sell: exchanges[key].Sell,
-    }));
+      console.log(response.data.exchanges);
+      const uniqueCurrencies = [
+        ...new Set(Object.keys(response.data.exchanges)),
+      ];
 
-    const uniqueCurrencies = [...new Set(Object.keys(exchanges))];
+      console.log('Available Currencies:', uniqueCurrencies);
+      setAvailableCurrencies(uniqueCurrencies);
 
-    console.log('Exchange Data:', formattedExchanges);
-    console.log('Available Currencies:', uniqueCurrencies);
-
-    setAvailableCurrencies(uniqueCurrencies);
-
-    console.log(exchanges);
-    setExchangeData(formattedExchanges);
-  };
-
-  useEffect(() => {
-    fetchExchangeRate();
-  }, []);
-
-  interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[];
-    data: TData[];
-  }
-
-  //TODO: uraditi preko implementiranog DataTable-a kada se sredi ruta
-  function DataTableTemporary<TData, TValue>({
-    columns,
-    data,
-  }: DataTableProps<TData, TValue>) {
-    const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-    });
-
-    return (
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-    );
-  }
+      const formattedExchanges = Object.values(response.data.exchanges).map(
+        (exchange) => ({
+          Base: exchange.Base,
+          Quote: exchange.Quote,
+          Buy: exchange.Buy,
+          Neutral: exchange.Neutral,
+          Sell: exchange.Sell,
+        })
+      );
+      setExchangeData(formattedExchanges);
+      return formattedExchanges;
+    },
+  });
 
   const convertCurrency = (
     amount: number,
@@ -188,103 +112,113 @@ const ExchangePage: React.FC = () => {
 
   return (
     <GuardBlock requiredUserType={'client'}>
-      <div className="p-8">
+      <div className="p-4">
         <Card className="max-w-[900px] mx-auto p-4">
           <CardContent className="rounded-lg overflow-hidden  items-center">
-            <DataTableTemporary columns={exchangeColumns} data={exchangeData} />
+            <DataTable
+              columns={exchangeColumns}
+              data={data ?? []}
+              isLoading={isLoading}
+              pageCount={0}
+              pagination={{ page, pageSize }}
+              onPaginationChange={(newPagination) => {
+                setPage(newPagination.page);
+                setPageSize(newPagination.pageSize);
+              }}
+            />
           </CardContent>
         </Card>
       </div>
-      <div className="p-8">
-        <Card className="max-w-[900px] mx-auto rounded-lg">
-          <CardContent className="flex items-center gap-4 p-10">
-            <div className="flex flex-col">
-              <span className="text-sm text-gray-400">Amount</span>
-              <Input
-                type="number"
-                defaultValue="1.00"
-                className="bg-gray-800 text-white w-32 rounded-md p-2"
-                onChange={(e) => {
-                  setAmount(parseFloat(e.target.value));
-                  setConvertedAmount(null);
-                }}
-              />
-            </div>
-            <div className="flex items-center gap-4">
-              <div className="flex flex-col">
-                <span className="text-sm text-gray-400">From</span>
-                <Select
-                  value={fromCurrency}
-                  onValueChange={(value) => {
-                    setFromCurrency(value);
+      <div className="p-4">
+        <Card className="max-w-[900px] mx-auto rounded-lg p-6">
+          <CardContent className="flex flex-col items-center gap-6 p-10">
+            <div className="flex items-center gap-6 w-full justify-center">
+              <div className="flex flex-col items-center">
+                <span className="text-sm text-gray-400 mb-1">Amount</span>
+                <Input
+                  type="number"
+                  defaultValue="1.00"
+                  className="bg-gray-800 text-white w-32 rounded-md p-2 text-center"
+                  onChange={(e) => {
+                    setAmount(parseFloat(e.target.value));
                     setConvertedAmount(null);
                   }}
-                >
-                  <SelectTrigger className="w-48 bg-gray-800 text-white rounded-md p-2">
-                    <SelectValue placeholder="Select currency" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {availableCurrencies.map((currency) => (
-                      <SelectItem key={currency} value={currency}>
-                        {currency}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
+                />
               </div>
-
-              <Button
-                variant="ghost"
-                size="icon"
-                onClick={swapCurrencies}
-                className="hover:bg-gray-700 rounded-md p-2"
-              >
-                <ArrowLeftRight className="text-white" />
-              </Button>
-
-              <div className="flex flex-col">
-                <span className="text-sm text-gray-400">To</span>
-                <Select
-                  value={toCurrency}
-                  onValueChange={(value) => {
-                    setToCurrency(value);
-                    setConvertedAmount(null);
+              <div className="flex items-center gap-4">
+                <div className="flex flex-col items-center">
+                  <span className="text-sm text-gray-400 mb-1">From</span>
+                  <Select
+                    value={fromCurrency}
+                    onValueChange={(value) => {
+                      setFromCurrency(value);
+                      setConvertedAmount(null);
+                    }}
+                  >
+                    <SelectTrigger className="w-48 bg-gray-800 text-white rounded-md p-2">
+                      <SelectValue placeholder="Select currency" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {availableCurrencies.map((currency) => (
+                        <SelectItem key={currency} value={currency}>
+                          {currency}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={swapCurrencies}
+                  className="hover:bg-gray-700 rounded-md p-2"
+                >
+                  <ArrowLeftRight className="text-white" />
+                </Button>
+                <div className="flex flex-col items-center">
+                  <span className="text-sm text-gray-400 mb-1">To</span>
+                  <Select
+                    value={toCurrency}
+                    onValueChange={(value) => {
+                      setToCurrency(value);
+                      setConvertedAmount(null);
+                    }}
+                  >
+                    <SelectTrigger className="w-48 bg-gray-800 text-white rounded-md p-2">
+                      <SelectValue placeholder="Select currency" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {availableCurrencies.map((currency) => (
+                        <SelectItem key={currency} value={currency}>
+                          {currency}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <Button
+                  onClick={() => {
+                    if (fromCurrency && toCurrency) {
+                      setConvertedAmount(
+                        convertCurrency(
+                          amount,
+                          fromCurrency,
+                          toCurrency,
+                          exchangeData
+                        )
+                      );
+                    }
                   }}
+                  variant={'default'}
                 >
-                  <SelectTrigger className="w-48 bg-gray-800 text-white rounded-md p-2">
-                    <SelectValue placeholder="Select currency" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {availableCurrencies.map((currency) => (
-                      <SelectItem key={currency} value={currency}>
-                        {currency}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
+                  Convert
+                </Button>
               </div>
-            </div>
-            <div className="flex flex-col">
-              <Button
-                onClick={() => {
-                  if (fromCurrency && toCurrency) {
-                    setConvertedAmount(
-                      convertCurrency(
-                        amount,
-                        fromCurrency,
-                        toCurrency,
-                        exchangeData
-                      )
-                    );
-                  }
-                }}
-                className="bg-blue-500 text-white px-6 py-2 rounded-md"
-              >
-                Convert
-              </Button>
             </div>
             {convertedAmount !== null && (
-              <div className="flec flex-row text-lg font-semibold text-white mt-4">
+              <div className="text-lg font-semibold text-white mt-6 text-center text-2xl">
                 {amount} {fromCurrency} = {convertedAmount} {toCurrency}
               </div>
             )}
