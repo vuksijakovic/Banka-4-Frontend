394fd1c22da14bbbd608946e32779e4146f0cfe6
IgorMijanovic <igor.mijanovic02@gmail.com>
2025-03-19 19:45:50 +0100

    Update exchange api

--- src/api/exchange.ts
+++ src/api/exchange.ts
@@ -1,7 +1,12 @@
 import { Axios } from 'axios';
 import { ExchangeDto } from './response/exchange';
 
-export const searchExchanges = async (client: Axios) => {
-  console.log('searchExchanges');
-  return client.get<ExchangeDto>('/exchange/exchange-rate');
+export const searchExchanges = async (
+  client: Axios,
+  rowsPerPage: number,
+  currentPage: number
+) => {
+  return client.get<ExchangeDto>('/exchange/exchange-rate', {
+    params: { size: rowsPerPage, page: currentPage },
+  });
 };
