86bd35225366b0ffcb3814c0bd771a8c47883f46
unknown <vsijakovic1421rn@raf.rs>
2025-03-19 22:46:06 +0100

    Fix loanColumns type

--- src/ui/dataTables/loans/loanColumns.tsx
+++ src/ui/dataTables/loans/loanColumns.tsx
@@ -1,7 +1,8 @@
-import { ColumnDef } from '@tanstack/react-table';
+import { ColumnDef, Row } from '@tanstack/react-table';
 import { LoanDto } from '@/api/response/loan';
+import { Button } from '@/components/ui/button';
 
-export const loanColumns: ColumnDef<LoanDto>[] = [
+export const loanColumns = (handleViewDetails: (arg0: any) => void, handleViewInstallments: (arg0: any) => void) => [
   {
     accessorKey: 'type',
     header: 'Type',
@@ -18,5 +19,28 @@ export const loanColumns: ColumnDef<LoanDto>[] = [
       const currencyCode = info.row.original.currency.code;
       return `${amount.toLocaleString()} ${currencyCode}`;
     },
+  },
+  {
+    id: 'actions',
+    header: 'Actions',
+    cell: ({ row }: { row: Row<LoanDto> }) => (
+      <div className="flex gap-2 justify-center items-center">
+        <Button
+          size="sm"
+          onClick={() => handleViewDetails(row.original)}
+        >
+          Details
+        </Button>
+        <Button
+          size="sm"
+          variant="outline"
+          onClick={() =>
+            handleViewInstallments(row.original.loanNumber)
+          }
+        >
+          Installments
+        </Button>
+      </div>
+    ),
   },
 ];
