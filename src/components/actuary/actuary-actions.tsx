1554fe52d2ee37624d665197cfefd232c623926d
IgorMijanovic <igor.mijanovic02@gmail.com>
2025-04-15 23:50:01 +0200

    Change function to mutation

--- src/components/actuary/actuary-actions.tsx
+++ src/components/actuary/actuary-actions.tsx
@@ -6,28 +6,40 @@ import { useHttpClient } from '@/context/HttpClientContext';
 import { resetUsedLimit } from '@/api/actuaries';
 import { toast } from 'sonner';
 import { ActuaryItem } from '@/types/actuary';
+import { useMutation } from '@tanstack/react-query';
 
 export function ActuaryActions({ item }: { item: ActuaryItem }) {
   const [isChangeLimitDialogOpen, setChangeLimitDialogOpen] = useState(false);
   const [isConfirmDialogOpen, setConfirmDialogOpen] = useState(false);
   const client = useHttpClient();
 
-  const handleConfirm = async () => {
-    try {
-      await resetUsedLimit(client, item.user.id);
+  const handleSubmit = () => {
+    resetLimit(item.user.id);
+  }
+
+  const { mutate: resetLimit } = useMutation({
+    mutationKey: ['actuaries'],
+    mutationFn: async (actuaryId: string) => {
+      await resetUsedLimit(client, actuaryId);
+    },
+    onSuccess: () => {
       toast.success('Used limit reset successfully');
-    } catch (error) {
+    },
+    onError: () => {
       toast.error('Failed to reset used limit');
-    } finally {
-      setConfirmDialogOpen(false);
-    }
-  };
+    },
+  });
 
   return (
     <div className="flex space-x-2">
-      <Button variant="outline" onClick={() => setChangeLimitDialogOpen(true)}>
-        Change Limit
-      </Button>
+      {item.actuary.limitAmount !== null && (
+        <Button
+          variant="outline"
+          onClick={() => setChangeLimitDialogOpen(true)}
+        >
+          Change Limit
+        </Button>
+      )}
       <Button variant="outline" onClick={() => setConfirmDialogOpen(true)}>
         Reset Limit
       </Button>
@@ -43,7 +55,7 @@ export function ActuaryActions({ item }: { item: ActuaryItem }) {
       {isConfirmDialogOpen && (
         <ConfirmDialog
           open={isConfirmDialogOpen}
-          onConfirm={handleConfirm}
+          onConfirm={handleSubmit}
           onCancel={() => setConfirmDialogOpen(false)}
           title="Reset Limit"
           description="Are you sure you want to reset the used limit?"
