b4b43efcd153d83028142a523d1948378234624a
Marko Kocic <markokocic2000@gmail.com>
2025-04-18 12:10:53 +0200

    Revert "api/taxes: add `userType` filter to tax summary endpoint"
    This reverts commit b7c0127f1bd2fe4d016d8d88bc0c9cf2a0eb54d4.

--- src/api/request/taxes.ts
+++ src/api/request/taxes.ts
@@ -1,7 +1,4 @@
-import { TaxableUserType } from '@/types/taxes';
-
 export interface TaxSummaryFilters {
   firstName: string;
   lastName: string;
-  userType: TaxableUserType;
 }
--- src/types/taxes.ts
+++
@@ -1,6 +0,0 @@
-export const ALL_TAXABLE_USER_TYPES_ = ['CLIENT', 'ACTUARY'] as const;
-
-export type TaxableUserType = (typeof ALL_TAXABLE_USER_TYPES_)[number];
-export const ALL_TAXABLE_USER_TYPES: TaxableUserType[] = [
-  ...ALL_TAXABLE_USER_TYPES_,
-];
